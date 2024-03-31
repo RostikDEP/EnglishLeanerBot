a9a71748952ec6d0a2ce76c5ea53d1639ec3e900
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-03-31 21:06:29 +0300

    Update database and created new field Sentence

---
+++ database/dbProcessor.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class dbProcessor:
+    def __init__(self, path):
+        self.db_path = path
+        self.db = sqlite3.connect(path)
+        self.cursor = self.db.cursor()
+
+
+    def AddWord(self, word_, translate, sentence, user):
+        self.cursor.execute(f"INSERT INTO words(word, translate, sentence, user, entered, complete) VALUES (?, ?, ?, ?, ?, ?)", (word_, translate, sentence ,user, 0, 0))
+        self.db.commit()
+
+
+if __name__ == "__main__":
+    dbProc = dbProcessor("../data/words.db")
+    dbProc.AddWord("Hello", "Hello", "Sentence","User")
--- handlers/user.py
+++ handlers/user.py
@@ -1,8 +1,8 @@
 from bot import dp
 from aiogram import F
-from  aiogram.types import Message
+from aiogram.types import Message
 
 
 @dp.message(F.text == "/start")
 async def welcome(message: Message):
-    await           message.reply("Вітаю. Цей бот допоможе у вивченні англійських слів!")
+    await message.reply("Вітаю. Цей бот допоможе у вивченні англійських слів!")
