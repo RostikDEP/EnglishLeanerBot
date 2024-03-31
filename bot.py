f29af9271b93f35eda45ccdd3eb0011c3123b1c1
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-03-31 09:11:33 +0300

    Created firs bot

--- bot.py
+++ bot.py
@@ -1,2 +1,23 @@
 import os
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+import asyncio
+
+load_dotenv()
+bot = Bot(os.getenv("TOKEN"))
+dp = Dispatcher()
+
+
+@dp.message(F.text == "/start")
+async  def start(message: Message):
+    await message.reply("Hello")
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
